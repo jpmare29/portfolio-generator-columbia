2916d66918a08d2f80953ab0b5befb1ec20fb2d5
jpmare29 <jpmmare@yahoo.com>
2022-04-17 12:11:36 -0400

    changed file tree

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const generatePage = require('./page-template.js');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2);
 
 const [name, github] = profileDataArgs;
